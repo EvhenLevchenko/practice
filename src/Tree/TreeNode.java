282f28ccf1aced5cc10c1e59288d8a69d5578581
artemsilac <artemsilac@gmail.com>
2017-12-08 15:12:55 +0200

    First Commit2

---
+++ src/Tree/TreeNode.java
@@ -0,0 +1,32 @@
+package Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public class Solution {
+
+        private long prevVal = (long) Integer.MIN_VALUE - 1;
+
+        public boolean isValidBST(TreeNode root) {
+            if (root == null)
+                return true;
+            if (!isValidBST(root.left))
+                return false;
+            if (!(prevVal < root.val))
+                return false;
+            prevVal = root.val;
+            return isValidBST(root.right);
+        }
+    }
+}
+
+
+
+
+
