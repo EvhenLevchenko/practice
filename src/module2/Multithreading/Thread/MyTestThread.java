b6b0494b3191f3c02bb08940df2a6f730ef6da56
artemsilac <artemsilac@gmail.com>
2017-12-13 17:11:30 +0200

    First Commit2

---
+++ src/module2/Multithreading/Thread/MyTestThread.java
@@ -0,0 +1,15 @@
+package module2.Multithreading.Thread;
+
+public class MyTestThread  extends Thread {
+    @Override
+    public void run() {
+        for (int i =10 ; i >0 ; i--) {
+            System.out.println(i);
+            try{
+               Thread.sleep(10);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/module2/Multithreading/Thread/Runnable.java
@@ -0,0 +1,5 @@
+package module2.Multithreading.Thread;
+
+public interface Runnable {
+    public void run();
+}
