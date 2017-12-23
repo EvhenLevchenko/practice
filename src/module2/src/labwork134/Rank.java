ddb46d7a4612a7f0ca2f53ab6b94ed00abbe7033
John <levjohn1999@gmail.com>
2017-12-23 21:50:37 +0200

    op

--- src/module2/src/labwork134/Rank.java
+++
@@ -1,5 +0,0 @@
-package labwork134;
-
-public enum Rank {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
-}
--- src/module2/src/labwork134/Suit.java
+++
@@ -1,5 +0,0 @@
-package labwork134;
-
-public enum Suit {
-    SPADE, DIAMOND, CLUB, HEART
-}
