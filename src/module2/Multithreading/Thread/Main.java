e97c8c4dfa0f1e271badcf424d2e1cd86616cb97
artemsilac <artemsilac@gmail.com>
2017-12-29 18:34:21 +0200

    Merge branch 'master' of D:\idea\progekt\Test1\practice with conflicts.

---
+++ src/module2/Multithreading/Thread/Main.java
@@ -0,0 +1,16 @@
+package module2.Multithreading.Thread;
+
+public class Main {
+    public static void main(String[] args) {
+
+     MyTestThread testThread=new MyTestThread();
+     testThread.start();
+
+     try {
+         Thread.sleep(10);
+     }catch (InterruptedException e){
+         e.printStackTrace();
+     }
+        System.out.print("Boom");
+    }
+}
