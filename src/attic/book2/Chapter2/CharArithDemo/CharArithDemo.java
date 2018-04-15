5d43be103f795ed9ff5ed8b1f31977dad790e2bc
John <levjohn1999@gmail.com>
2018-04-15 11:22:57 +0300

    a

---
+++ src/attic/book2/Chapter2/CharArithDemo/CharArithDemo.java
@@ -0,0 +1,38 @@
+package attic.book2.Chapter2.CharArithDemo;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        CharArithDemo test = new CharArithDemo();
+        System.out.println(test.toString());
+        char ch;
+        ch = 'X';
+//        System.out.println("ch содержит " + ch);
+//
+//        ch++;//инкрементировать переменную ch
+//        System.out.println("теперь ch содержит " + ch);
+//
+//        ch++;
+//        System.out.println("теперь ch содержит " + ch);
+//        ch++;
+//        System.out.println("теперь ch содержит " + ch);
+//        ch++;
+//        System.out.println("1"+2+3);
+
+
+        int b = 10;
+        int h = 10;
+        int v = 10;
+        int n = 10;
+
+        int c = b--;
+        int o = --h;
+        int p = ++v;
+        int l = n++;
+
+        System.out.println(c);
+        System.out.println(o);
+        System.out.println(p);
+        System.out.println(l);
+
+    }
+}
